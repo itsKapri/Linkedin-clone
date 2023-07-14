9b08f6cda12f6e9865424b1573c89d99af5aae6f
नीलेश <70235074+itsKapri@users.noreply.github.com>
2023-07-14 21:59:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import React from 'react';
-// import Header from './Component/Header/Header';
-// import Sidebar from './Component/Sidebar/Sidebar';
-// import Feed from './Component/Feed/Feed';
-// import Widgets from './Component/Widgets/Widgets';
 import {
   createBrowserRouter,
   RouterProvider,
@@ -23,14 +19,6 @@ function App() {
   return (
     
     <div className="app_wrapper">
-    {/* <Header/>
-    <div className="app_body">
-      <Sidebar/>
-      <Feed/>
-      <Widgets/>
-    </div>
-    */}
-   
     <RouterProvider router={router} />
     </div>
     
